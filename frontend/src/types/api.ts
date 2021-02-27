ef9770572118f9d87b0c6e932907e589c04f1774
Felipe Borges <lipex360@gmail.com>
2021-02-27 18:59:02 -0300

    Section Header fetched

--- frontend/src/types/api.ts
+++ frontend/src/types/api.ts
@@ -1,8 +1,19 @@
-export type LogoProps = {
+export type ImageProps = {
   alternativeText: string
   url: string
 }
 
+export type HeaderProps = {
+  title: string
+  description: string
+  button: {
+    label: string
+    url: string
+  }
+  image: ImageProps
+}
+
 export type LandingPageProps = {
-  logo: LogoProps
+  logo: ImageProps
+  header: HeaderProps
 }
