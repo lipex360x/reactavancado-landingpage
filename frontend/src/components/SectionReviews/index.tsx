a84d4f5b545914224461aea5245818be43134003
Felipe Borges <lipex360@gmail.com>
2021-02-27 21:43:16 -0300

    Reviews section added

--- frontend/src/components/SectionReviews/index.tsx
+++ frontend/src/components/SectionReviews/index.tsx
@@ -5,16 +5,18 @@ import Container from 'components/Container'
 import Heading from 'components/Heading'
 import ReviewCard from 'components/ReviewCard'
 
-import reviews from './content'
 import * as S from './styles'
+import { SectionReviewsProps } from 'types/api'
 
 const settings = {
   dots: true,
   arrows: false,
   slidesToShow: 2,
-  infinite: false,
+  infinite: true,
   speed: 500,
-  rows: 3,
+  rows: 2,
+  autoplay: true,
+  autoplaySpeed: 5000,
   slidesPerRow: 1,
   slidesToScroll: 2,
   responsive: [
@@ -30,18 +32,18 @@ const settings = {
   ]
 }
 
-const SectionReviews = () => (
+const SectionReviews = ({ title, reviews }: SectionReviewsProps) => (
   <Container>
-    <Heading reverseColor>Junte-se a mais de 200 mil alunos</Heading>
+    <Heading reverseColor>{title}</Heading>
 
     <S.Content>
       <Slider {...settings}>
-        {reviews.map(({ name, image, description }, index) => (
+        {reviews.map(({ name, photo, text }, index) => (
           <ReviewCard
             key={index}
             name={name}
-            image={image}
-            description={description}
+            photo={photo}
+            text={text}
             id={index}
           />
         ))}
